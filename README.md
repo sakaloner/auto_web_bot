7834787707b9302945123e51c2cfa8c6b34a1263
sakaloner <andresf40y0@gmail.com>
2023-04-11 20:58:59 -0500

    basic proyect setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # auto_web_bot
 A bot for creating webpages
+
+
+# Installation
+python -m venv env
+
+source env/bin/activate
+
+pip install -r requirements.txt
+
--- main.py
+++ main.py
@@ -6,45 +6,26 @@ import openai
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt2 = """
-<!-- Create a web page with the title 'Vegan Fast Food'.
-Use Tailwind CSS to style the page. -->
-<!DOCTYPE html>
-"""
-
-prompt3 = """
+prompt = """
 <!-- Create a landing page for a crypto currency exchange with html -->
 <!DOCTYPE html>
 """
-
-response = openai.Completion.create(
-    engine="code-davinci-002",
-    prompt=prompt3,
-    temperature=0.3,
-    max_tokens=2000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
+responseChat = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
 )
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+
 
 print(response.choices[0].text)
+print(responseChat.choices[0].message.content)
+## Guardar respuesta en un archivo
 with open("../index.html", "w") as f:
     full_text = response.choices[0].text
     f.write(full_text)
-
-newPrompt = response.choices[0].text + prompt3
-
-response2 = openai.Completion.create(
-    engine="code-davinci-002",
-    prompt=newPrompt,
-    temperature=0.3,
-    max_tokens=2000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-)
-
-print(response2.choices[0].text)
-with open("../styles.css", "w") as f:
-    full_text = response2.choices[0].text
-    f.write(full_text)
+# 
